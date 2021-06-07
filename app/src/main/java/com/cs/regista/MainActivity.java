cc26eb71d76fed4cf8888368764f0d5f6fed3cda
Dev-Rishav <rishavchakraborty94@gmail.com>
2021-06-07 05:38:34 +0530

    Bind Navigation Drawer Menu to the left and introduce material design

--- app/src/main/java/com/cs/regista/MainActivity.java
+++ app/src/main/java/com/cs/regista/MainActivity.java
@@ -1,19 +1,35 @@
 package com.cs.regista;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        //Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        //Toolbar
         setSupportActionBar(toolbar);
-
+        //Navigation Drawer Menu
+        ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.nav_bar_open,R.string.nav_bar_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
