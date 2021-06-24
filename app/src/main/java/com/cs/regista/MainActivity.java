03e907b2278d176c7ac36440825b1694ec4d7844
Rishav Chakraborty <rishavchakraborty94@gmail.com>
2021-06-25 01:10:04 +0530

    add stduent using the main FAB
    <!-- Just the interface part -->

--- app/src/main/java/com/cs/regista/MainActivity.java
+++ app/src/main/java/com/cs/regista/MainActivity.java
@@ -1,13 +1,18 @@
 package com.cs.regista;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
+        fab = findViewById(R.id.main_fab);
         //Toolbar
         setSupportActionBar(toolbar);
         //Navigation Drawer Menu
@@ -33,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.nav_bar_open,R.string.nav_bar_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        //Dialogue for main floating action button
+        fab.setOnClickListener(v-> showDialog());
     }
 
     @Override
@@ -44,5 +54,13 @@ public class MainActivity extends AppCompatActivity {
         }
         else
             super.onBackPressed();
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.student_dialog,null);
+        builder.setView(view);
+
+        builder.create().show();
     }
 }
